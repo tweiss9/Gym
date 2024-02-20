4e45d471e3eef54379f1f3099649b77115d8ea60
tweiss9 <tylerhweiss@gmail.com>
2024-02-19 22:03:59 -0500

    popup design

--- lib/widgets/popup.dart
+++ lib/widgets/popup.dart
@@ -25,53 +25,112 @@ class Popup {
       barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text(title),
+          backgroundColor: Colors.white,
+          title: Center(
+              child: Text(
+            title,
+            style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          )),
           content: isNumber || isText
               ? (isNumber
                   ? TextField(
                       controller: textController,
-                      decoration: InputDecoration(hintText: contentController),
+                      decoration: InputDecoration(
+                        hintText: contentController,
+                        contentPadding:
+                            const EdgeInsets.symmetric(horizontal: 10),
+                        focusedBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                      ),
                       keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
+                      cursorColor: Colors.black,
                     )
                   : TextField(
                       controller: textController,
-                      decoration: InputDecoration(hintText: contentController),
+                      decoration: InputDecoration(
+                        hintText: contentController,
+                        contentPadding:
+                            const EdgeInsets.symmetric(horizontal: 10),
+                        focusedBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                      ),
+                      textAlign: TextAlign.center,
+                      cursorColor: Colors.black,
                     ))
-              : Text(contentController),
+              : Text(contentController,
+                  style: const TextStyle(fontSize: 18),
+                  textAlign: TextAlign.center),
           actions: <Widget>[
-            TextButton(
-              child: Text(cancelButtonText),
-              onPressed: () {
-                Navigator.of(context).pop();
-                textController.clear();
-              },
-            ),
-            TextButton(
-              child: Text(okButtonText),
-              onPressed: () {
-                String? textInput =
-                    isText || isNumber ? textController.text.trim() : null;
-                if (textController.text.trim() == "" && isText) {
-                  ErrorHandler.showError(context, 'Input cannot be empty');
-                  return;
-                }
-                if (isNumber) {
-                  try {
-                    int.parse(textInput!);
-                  } catch (e) {
-                    ErrorHandler.showError(context, 'Input must be a number');
-                    return;
-                  }
-                }
-                if (isNumber && int.parse(textInput!) <= 0) {
-                  ErrorHandler.showError(
-                      context, 'Input must be greater than 0');
-                  return;
-                }
-                onOkPressed(textInput: textInput);
-                Navigator.of(context).pop();
-                textController.clear();
-              },
+            Column(
+              children: [
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 40),
+                      ElevatedButton(
+                        onPressed: () {
+                          String? textInput = isText || isNumber
+                              ? textController.text.trim()
+                              : null;
+                          if (textController.text.trim() == "" && isText) {
+                            ErrorHandler.showError(
+                                context, 'Input cannot be empty');
+                            return;
+                          }
+                          if (isNumber) {
+                            try {
+                              int.parse(textInput!);
+                            } catch (e) {
+                              ErrorHandler.showError(
+                                  context, 'Input must be a number');
+                              return;
+                            }
+                          }
+                          if (isNumber && int.parse(textInput!) <= 0) {
+                            ErrorHandler.showError(
+                                context, 'Input must be greater than 0');
+                            return;
+                          }
+                          onOkPressed(textInput: textInput);
+                          Navigator.of(context).pop();
+                          textController.clear();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.green,
+                          fixedSize: const Size(300, 40),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          ),
+                        ),
+                        child: Text(okButtonText,
+                            style: const TextStyle(fontSize: 18)),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          textController.clear();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.red,
+                          fixedSize: const Size(300, 40),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          ),
+                        ),
+                        child: Text(cancelButtonText,
+                            style: const TextStyle(fontSize: 18)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         );
